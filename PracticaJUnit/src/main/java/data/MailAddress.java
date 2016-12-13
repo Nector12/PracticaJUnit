7b9b4282cb7597ba8f6642137c3018abddee0c5d
Nector12 <rarquesvall@gmail.com>
2016-12-13 19:59:58 +0100

    Created class MailAddress

---
+++ PracticaJUnit/src/main/java/data/MailAddress.java
@@ -0,0 +1,52 @@
+package data;
+
+import java.util.Objects;
+
+/**
+ * Describes a mail address
+ *
+ * @author Nector
+ */
+final public class MailAddress {
+    
+    private final String address;
+
+    public MailAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public int hashCode() {
+        return address.hashCode();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final MailAddress other = (MailAddress) obj;
+        if (!Objects.equals(this.address, other.address)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "MailAddress{" + "address=" + address + '}';
+    }
+    
+    
+    
+}
