a5879acfdd7020566404393397f07b5a018ad160
Nector12 <rarquesvall@gmail.com>
2017-01-01 20:32:12 +0100

    Created skeletton of class VotingMachine

---
+++ PracticaJUnit/src/main/java/kiosk/VotingMachine.java
@@ -0,0 +1,69 @@
+package kiosk;
+
+import data.MailAddress;
+import data.Vote;
+import services.MailerService;
+import services.SignatureService;
+import services.ValidationService;
+import services.VotePrinter;
+import services.VotesDB;
+
+/**
+ *
+ * @author rav3
+ */
+/**
+ * Implements a simplification of Use Case: Emit Vote
+ */
+public class VotingMachine {
+
+    private ValidationService validationService;
+    private VotePrinter votePrinter;
+    private VotesDB votesDB;
+    private SignatureService signatureService;
+    private MailerService mailerService;
+
+    public VotingMachine() {
+
+    }
+
+    public void setValidationService(ValidationService validationService) {
+        this.validationService = validationService;
+    }
+
+    public void setVotePrinter(VotePrinter votePrinter) {
+        this.votePrinter = votePrinter;
+    }
+
+    public void setVotesDB(VotesDB votesDB) {
+        this.votesDB = votesDB;
+    }
+
+    public void setSignatureService(SignatureService signatureService) {
+        this.signatureService = signatureService;
+    }
+
+    public void setMailerService(MailerService mailerService) {
+        this.mailerService = mailerService;
+    }
+
+    public void activateEmission(ActivationCard card)
+            throws IllegalStateException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public boolean canVote() {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void vote(Vote vote)
+            throws IllegalStateException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+    public void sendReceipt(MailAddress mailAddress)
+            throws IllegalStateException {
+        throw new UnsupportedOperationException("Not implemented yet");
+    }
+
+}
