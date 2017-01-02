5f6ab3ac6ee277934e4ee1946dbe23d50693cbed
Nector12 <rarquesvall@gmail.com>
2017-01-02 19:39:59 +0100

    Created test for 'activateEmission'. Test PASSED

--- PracticaJUnit/src/test/java/kiosk/VotingMachineActivateEmisionTests.java
+++ PracticaJUnit/src/test/java/kiosk/VotingMachineActivateEmisionTests.java
@@ -1,5 +1,6 @@
 package kiosk;
 
+import mocks.ForbiddenValidationService;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -12,7 +13,44 @@ public class VotingMachineActivateEmisionTests {
     private VotingMachine votingMachine;
 
     public VotingMachineActivateEmisionTests() {
+        votingMachine = new VotingMachine();
+    }
+    
+    @Test
+    public void correctlyActivatedEmission() {
+        votingMachine.setValidationService(new ValidationServiceOkay());
+        votingMachine.activateEmission(new ActivationCard("activation_code"));
+        assertTrue(votingMachine.isActivated());
+    }
+    
+    @Test
+    public void errorActivatingEmissionInvalidCode() {
+        votingMachine.setValidationService(new ValidationServiceWrong());
+        votingMachine.activateEmission(new ActivationCard("invalid_code"));
+        assertFalse(votingMachine.isActivated());
     }
 
+    /*
+    Mock implementations of ValidationService
+    */
+    private static class ValidationServiceOkay 
+            extends ForbiddenValidationService {
 
+        @Override
+        public boolean validate(ActivationCard card) {
+            return true;
+        }
+        
+    }
+
+    private static class ValidationServiceWrong 
+            extends ForbiddenValidationService {
+
+        @Override
+        public boolean validate(ActivationCard card) {
+            return false;
+        }
+        
+    }
+    
 }
