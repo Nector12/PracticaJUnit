fecdc9a475f075eae83f585d176ab2ea122d2859
Nector12 <rarquesvall@gmail.com>
2017-01-02 20:50:54 +0100

    Tests for canVote(), tests PASSED

--- PracticaJUnit/src/test/java/kiosk/VotingMachineCanVoteTests.java
+++ PracticaJUnit/src/test/java/kiosk/VotingMachineCanVoteTests.java
@@ -1,5 +1,9 @@
-
 package kiosk;
+
+import mocks.ForbiddenValidationService;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
 
 /**
  *
@@ -7,4 +11,36 @@ package kiosk;
  */
 public class VotingMachineCanVoteTests {
 
+    private VotingMachine votingMachine;
+
+    public VotingMachineCanVoteTests() {
+    }
+    
+    @Before
+    public void setUpVotingMachine() {
+        votingMachine = new VotingMachine();
+        votingMachine.setValidationService(new ValidationServiceOkay());
+    }
+    
+    @Test
+    public void voterCanVote() {
+        votingMachine.activateEmission(new ActivationCard("valid_code"));
+        assertTrue(votingMachine.canVote());
+    }
+    
+    @Test
+    public void voterCannotVoteEmissionNotActivated() {
+        assertFalse(votingMachine.canVote());
+    }
+    
+    private static class ValidationServiceOkay
+            extends ForbiddenValidationService {
+
+        @Override
+        public boolean validate(ActivationCard card) {
+            return true;
+        }
+
+    }
+    
 }
