f9313d4fc663e642388c85b9d0d1446247935ff7
Nector12 <rarquesvall@gmail.com>
2017-01-04 13:33:20 +0100

    Created test skeletton for 'sendReceipt()' test Using new mocks

--- PracticaJUnit/src/test/java/kiosk/VotingMachineSendReceiptTests.java
+++ PracticaJUnit/src/test/java/kiosk/VotingMachineSendReceiptTests.java
@@ -1,10 +1,45 @@
-
 package kiosk;
+
+import data.Vote;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import services.VotePrinter;
 
 /**
  *
  * @author rav3
  */
 public class VotingMachineSendReceiptTests {
+
+    VotingMachine votingMachine;
+
+    public VotingMachineSendReceiptTests() {
+    }
+
+    @Before
+    public void setUpVotingMachine() {
+        this.votingMachine = new VotingMachine();
+    }
+
+    @Test
+    public void cannotSendIfMachineNotActivated() {
+
+    }
+
+    @Test
+    public void cannotSendIfNotVoted() {
+
+    }
+
+    @Test
+    public void sentToCorrectAddress() {
+
+    }
+
+    @Test
+    public void sentCorrectSignature() {
+    
+    }
 
 }
