d0d4ca368ba98a00575a826c9ca7da55d2c16872
Nector12 <rarquesvall@gmail.com>
2017-01-04 14:10:04 +0100

    Created tests for 'sendReceipt()' TESTS PASSED

--- PracticaJUnit/src/test/java/kiosk/VotingMachineSendReceiptTests.java
+++ PracticaJUnit/src/test/java/kiosk/VotingMachineSendReceiptTests.java
@@ -1,6 +1,13 @@
 package kiosk;
 
+import data.MailAddress;
+import data.Signature;
 import data.Vote;
+import mocks.MailerServiceFake;
+import mocks.SignatureServiceFake;
+import mocks.ValidationServiceOkay;
+import mocks.VotePrinterFake;
+import mocks.VotesDBFake;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -20,26 +27,51 @@ public class VotingMachineSendReceiptTests {
     @Before
     public void setUpVotingMachine() {
         this.votingMachine = new VotingMachine();
+        this.votingMachine.setValidationService(new ValidationServiceOkay());
+        this.votingMachine.setVotesDB(new VotesDBFake());
+        this.votingMachine.setVotePrinter(new VotePrinterFake());
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void cannotSendIfMachineNotActivated() {
-
+        this.votingMachine.sendReceipt(new MailAddress("any_address"));        
     }
 
-    @Test
+    @Test(expected = IllegalStateException.class)
     public void cannotSendIfNotVoted() {
-
+        this.votingMachine.activateEmission(new ActivationCard("valid_code"));
+        this.votingMachine.sendReceipt(new MailAddress("any_address"));
     }
 
     @Test
     public void sentToCorrectAddress() {
-
+        MailerServiceFake mailerService = new MailerServiceFake();
+        SignatureServiceFake signatureService = new SignatureServiceFake();
+        this.votingMachine.setMailerService(mailerService);
+        this.votingMachine.setSignatureService(signatureService);
+        
+        MailAddress address = new MailAddress("any_address");
+        
+        this.votingMachine.activateEmission(new ActivationCard("valid_code"));
+        this.votingMachine.vote(new Vote("any_party"));
+        this.votingMachine.sendReceipt(address);
+        assertEquals(address, mailerService.address);
     }
 
     @Test
     public void sentCorrectSignature() {
-    
+        MailerServiceFake mailerService = new MailerServiceFake();
+        SignatureServiceFake signatureService = new SignatureServiceFake();
+        this.votingMachine.setMailerService(mailerService);
+        this.votingMachine.setSignatureService(signatureService);
+        
+        Signature expected = new Signature(
+                new byte[]{1,2,3,4,5,6,7,8,9});
+        
+        this.votingMachine.activateEmission(new ActivationCard("valid_code"));
+        this.votingMachine.vote(new Vote("any_party"));
+        this.votingMachine.sendReceipt(new MailAddress("any_address"));
+        assertEquals(expected, mailerService.signature);
     }
 
 }
